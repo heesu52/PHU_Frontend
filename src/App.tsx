9c3a25c999b7b048fa7370b9b3e640705a1030ea
Heesu Jang <heesu52@naver.com>
2024-09-24 14:22:50 +0900

    design: 기본 레이아웃 & css속성 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <span>메인 페이지</span>
-    </>
+    <div className='w-[600px] h-screen mx-auto font-[Pretendard]'>
+      <div className="h-full border border-custom-softgrey">
+        <span>메인 페이지</span>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", //수정
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // 소스 파일 경로 설정
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue" : "#3C8ACF",
+        "custom-skyblue" : "#C5D9FA",
+        "custom-softblue" : "#F3F7FE",
+        "custom-orange" :"#ED930C",
+        "custom-softgrey" : "#e6e6e6",
+        "custom-grey" : "#D6D6D6",
+        "custom-darkgrey" : "#A6A6A6",
+        "custom-indigo" : "#1E4061",
+        "custom-yellow" : "#F6D468"
+      },
+    },
   },
   plugins: [],
- }
-
+}
